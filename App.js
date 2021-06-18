2874c9eabdbd542c5f8f50fe6c5129cfff6b41fb
naihsienchen <71660637+naihsienchen@users.noreply.github.com>
2021-06-18 17:12:05 -0400

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
+  console.log("App executed");
+  console.log(require("./assets/favicon.png"));
+  const handlePress = () => console.log("Text Pressed!");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello React Native! </Text>
+      
+      <Button 
+        title="Click Me" 
+        onPress={()=> console.log("tapped")}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +24,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    
   },
 });
